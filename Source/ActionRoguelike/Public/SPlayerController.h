93e56171cd6ac6f416ee2582208e37e861d7776a
Borja Santiago <borjasantiagog@gmail.com>
2023-02-27 12:30:38 +0100

    Lecture 24
    - Creating Main Menus with UMG.
    - Creating In-Game Menus.

--- Source/ActionRoguelike/Public/SPlayerController.h
+++ Source/ActionRoguelike/Public/SPlayerController.h
@@ -10,6 +10,8 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnPawnChanged, APawn*, NewPawn);
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnPlayerStateChanged, APlayerState*, NewPlayerState);
 
+class UUserWidget;
+
 /**
  * 
  */
@@ -19,11 +21,22 @@ class ACTIONROGUELIKE_API ASPlayerController : public APlayerController
 	GENERATED_BODY()
 
 protected:
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<UUserWidget> PauseMenuClass;
+
+	UPROPERTY()
+	UUserWidget* PauseMenuInstance;
+
 	UPROPERTY(BlueprintAssignable)
 	FOnPawnChanged OnPawnChanged;
 
 	UPROPERTY(BlueprintAssignable)
 	FOnPlayerStateChanged OnPlayerStateReceived;
+
+	UFUNCTION(BlueprintCallable)
+	void TogglePauseMenu();
+
+	void SetupInputComponent() override;
 
 	virtual void SetPawn(APawn* InPawn) override;
 
